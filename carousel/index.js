88138c17ef18362265edf9e7d8de49a73c5cb4bf
Jang Jaehoon <idlinlee12@gmail.com>
2017-12-28 18:11:56 +0900

    test

--- carousel/index.js
+++ carousel/index.js
@@ -8,48 +8,6 @@ const right__btn = document.querySelector('.right__btn');
 const carousel__list = document.querySelectorAll('.carousel__content');
 
 
-
-for (let i = 0; i < carousel__list.length; i++) {
-    carousel__list[i].classList.add(`carousel__content__${i + 1}`);
-    // 첫번째 컨텐츠는 left = 0
-    if (carousel__list[i] === document.querySelector(`.carousel__content__1`)) {
-        document.querySelector(`.carousel__content__${i + 1}`).style.left = `0`;
-    } //마지막 컨텐츠는 left = -400px
-    else if (carousel__list[i] === document.querySelector(`.carousel__content__${carousel__list.length}`)) {
-        document.querySelector(`.carousel__content__${i + 1}`).style.left = `-400px`;
-    } //나머지 컨텐츠 만약 컨텐츠가 두번째면 400px 세번째면 800px ...
-    else {
-        document.querySelector(`.carousel__content__${i + 1}`).style.left += `${400*i}px`;
-    }
-}
-// 클릭 이벤트
-carousel__btn.forEach(item => {
-    item.addEventListener('click', function () {
-        if (item.classList.contains('left__btn')) {
-            carouselEvent()
-            console.log('left');
-        } else if (item.classList.contains('right__btn')) {
-            console.log(carousel__list.length);
-        };
-    });
-    
-});
-
-function carouselEvent() {
-    for (let i = 0; i < carousel__list.length; i++) {
-        carousel__list[i].classList.remove(`carousel__content__${i + 1}`);
-        // 왼쪽 버튼을 클릭하면 첫번째 요소를 바꾼다.
-        if (carousel__list[i] === carousel__list[0]) {
-            carousel__list[i].classList.add(`carousel__content__${carousel__list.length}`);
-            carousel__list[i].style.left = `-400px`;    
-        } // 두번째 요소를 0으로 바꾼다. 
-        else if (carousel__list[i] === carousel__list[1]) {
-            carousel__list[i].classList.add(`carousel__content__${i}`);
-            carousel__list[i].style.left = `0px`;
-        }
-        else {
-            carousel__list[i].classList.add(`carousel__content__${i}`);
-            carousel__list[i].style.left = `${400 * (i - 1)}px`;
-        }
-    }
+for (let i = 0; i < 3; i++) {
+    carousel__list.classList.a
 }
